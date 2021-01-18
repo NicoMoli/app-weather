bc093fcc9c8b27ad7379db2f62b94df2e8f661ea
Nicolás Molina <nicolas.molina@tecso.coop>
2021-01-18 09:03:33 -0300

    Agregamos tests a la app.

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  moduleNameMapper: {
+   
+    '^styled-components$':
+      '<rootDir>/node_modules/styled-components/native/dist/styled-components.native.cjs.js',
+  },
+
+};
